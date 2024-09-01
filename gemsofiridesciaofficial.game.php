beaceb19f2c7fdb666257a517601b65ebea044c3
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2024-09-01 17:20:06 -0300

    chore: set up deck for tiles

--- gemsofiridesciaofficial.game.php
+++ gemsofiridesciaofficial.game.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *------
  * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
@@ -14,32 +15,21 @@
  *
  * In this PHP file, you are going to defines the rules of the game.
  */
+
 declare(strict_types=1);
 
 require_once(APP_GAMEMODULE_PATH . "module/table/table.game.php");
 
 class GemsOfIridesciaOfficial extends Table
 {
-    /**
-     * Your global variables labels:
-     *
-     * Here, you can assign labels to global variables you are using for this game. You can use any number of global
-     * variables with IDs between 10 and 99. If your game has options (variants), you also have to associate here a
-     * label to the corresponding ID in `gameoptions.inc.php`.
-     *
-     * NOTE: afterward, you can get/set the global variables with `getGameStateValue`, `setGameStateInitialValue` or
-     * `setGameStateValue` functions.
-     */
     public function __construct()
     {
         parent::__construct();
 
-        $this->initGameStateLabels([
-            "my_first_global_variable" => 10,
-            "my_second_global_variable" => 11,
-            "my_first_game_variant" => 100,
-            "my_second_game_variant" => 101,
-        ]);
+        $this->initGameStateLabels([]);
+
+        $this->tiles = $this->getNew("module.common.deck");
+        $this->tiles->init("tile");
     }
 
     /**
@@ -132,13 +122,14 @@ class GemsOfIridesciaOfficial extends Table
      *
      * The action method of state `nextPlayer` is called everytime the current game state is set to `nextPlayer`.
      */
-    public function stNextPlayer(): void {
+    public function stNextPlayer(): void
+    {
         // Retrieve the active player ID.
         $player_id = (int)$this->getActivePlayerId();
 
         // Give some extra time to the active player when he completed an action
         $this->giveExtraTime($player_id);
-        
+
         $this->activeNextPlayer();
 
         // Go to another gamestate
@@ -159,21 +150,21 @@ class GemsOfIridesciaOfficial extends Table
      */
     public function upgradeTableDb($from_version)
     {
-//       if ($from_version <= 1404301345)
-//       {
-//            // ! important ! Use DBPREFIX_<table_name> for all tables
-//
-//            $sql = "ALTER TABLE DBPREFIX_xxxxxxx ....";
-//            $this->applyDbUpgradeToAllDB( $sql );
-//       }
-//
-//       if ($from_version <= 1405061421)
-//       {
-//            // ! important ! Use DBPREFIX_<table_name> for all tables
-//
-//            $sql = "CREATE TABLE DBPREFIX_xxxxxxx ....";
-//            $this->applyDbUpgradeToAllDB( $sql );
-//       }
+        //       if ($from_version <= 1404301345)
+        //       {
+        //            // ! important ! Use DBPREFIX_<table_name> for all tables
+        //
+        //            $sql = "ALTER TABLE DBPREFIX_xxxxxxx ....";
+        //            $this->applyDbUpgradeToAllDB( $sql );
+        //       }
+        //
+        //       if ($from_version <= 1405061421)
+        //       {
+        //            // ! important ! Use DBPREFIX_<table_name> for all tables
+        //
+        //            $sql = "CREATE TABLE DBPREFIX_xxxxxxx ....";
+        //            $this->applyDbUpgradeToAllDB( $sql );
+        //       }
     }
 
     /*
@@ -212,58 +203,66 @@ class GemsOfIridesciaOfficial extends Table
         return "gemsofiridesciaofficial";
     }
 
-    /**
-     * This method is called only once, when a new game is launched. In this method, you must setup the game
-     *  according to the game rules, so that the game is ready to be played.
-     */
     protected function setupNewGame($players, $options = [])
     {
-        // Set the colors of the players with HTML color code. The default below is red/green/blue/orange/brown. The
-        // number of colors defined here must correspond to the maximum number of players allowed for the gams.
         $gameinfos = $this->getGameinfos();
         $default_colors = $gameinfos['player_colors'];
 
-	foreach ($players as $player_id => $player) {
-		// Now you can access both $player_id and $player array
-		$query_values[] = vsprintf("('%s', '%s', '%s', '%s', '%s')", [
-			$player_id,
-			array_shift($default_colors),
-			$player["player_canal"],
-			addslashes($player["player_name"]),
-			addslashes($player["player_avatar"]),
-		]);
-	}
+        foreach ($players as $player_id => $player) {
+            $query_values[] = vsprintf("('%s', '%s', '%s', '%s', '%s')", [
+                $player_id,
+                array_shift($default_colors),
+                $player["player_canal"],
+                addslashes($player["player_name"]),
+                addslashes($player["player_avatar"]),
+            ]);
+        }
 
-	// Create players based on generic information.
-	//
-	// NOTE: You can add extra field on player table in the database (see dbmodel.sql) and initialize
-	// additional fields directly here.
-	static::DbQuery(
-		sprintf(
-			"INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar) VALUES %s",
-			implode(",", $query_values)
-		)
-	);
+        static::DbQuery(
+            sprintf(
+                "INSERT INTO player (player_id, player_color, player_canal, player_name, player_avatar) VALUES %s",
+                implode(",", $query_values)
+            )
+        );
 
         $this->reattributeColorsBasedOnPreferences($players, $gameinfos["player_colors"]);
         $this->reloadPlayersBasicInfos();
 
-        // Init global values with their initial values.
+        $tiles = [];
+        foreach ($this->tiles_info as $tile_id => $tile_info) {
+            $terrain_id = $tile_info["terrain"];
+            $gem_id = $tile_info["gem"];
+            $effect_id = $tile_info["effect"];
 
-        // Dummy content.
-        $this->setGameStateInitialValue("my_first_global_variable", 0);
+            $tiles[] = ["type" => $terrain_id, "type_arg" => $tile_id, "nbr" => 1];
+        }
 
-        // Init game statistics.
-        //
-        // NOTE: statistics used in this file must be defined in your `stats.inc.php` file.
+        $this->tiles->createCards($tiles, "deck");
 
-        // Dummy content.
-        // $this->initStat("table", "table_teststat1", 0);
-        // $this->initStat("player", "player_teststat1", 0);
+        $hex = 1;
+        foreach ($this->terrains_info as $terrain_id => $terrain) {
+            $tilesOfTerrain = $this->tiles->getCardsOfTypeInLocation($terrain_id, null, "deck");
+            $k_tilesOfTerrain = array_keys($tilesOfTerrain);
 
-        // TODO: Setup the initial game situation here.
+            $terrainName =  $terrain["name"];
+            $this->tiles->moveCards($k_tilesOfTerrain, $terrainName);
+            $this->tiles->shuffle($terrainName);
 
-        // Activate first player once everything has been initialized and ready.
+            for ($i = 1; $i <= count($tilesOfTerrain); $i++) {
+                $this->tiles->pickCardForLocation($terrainName, strval($hex));
+                $hex++;
+            }
+
+            foreach ($tilesOfTerrain as $card_id => $card) {
+                $tile_id = $card["type_arg"];
+
+                $gem_id = $this->tiles_info[$tile_id]["gem"];
+                $effect_id = $this->tiles_info[$tile_id]["effect"];
+
+                $this->DbQuery("UPDATE tile SET card_type_arg=$gem_id, card_location_arg=$effect_id WHERE card_id=$card_id");
+            }
+        }
+
         $this->activeNextPlayer();
     }
 
@@ -289,11 +288,10 @@ class GemsOfIridesciaOfficial extends Table
 
         if ($state["type"] === "activeplayer") {
             switch ($state_name) {
-                default:
-                {
-                    $this->gamestate->nextState("zombiePass");
-                    break;
-                }
+                default: {
+                        $this->gamestate->nextState("zombiePass");
+                        break;
+                    }
             }
 
             return;
--- material.inc.php
+++ material.inc.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *------
  * BGA framework: Gregory Isabelli & Emmanuel Colin & BoardGameArena
@@ -19,20 +20,369 @@
  *
  */
 
-
-// Example:
-
-$this->card_types = [
+$this->gems_info = [
     1 => [
-        "card_name" => clienttranslate('Troll'), // ...
+        "name" => "Amethyst",
+        "tr_label" => clienttranslate("Amethyst")
     ],
     2 => [
-        "card_name" => clienttranslate('Goblin'), // ...
+        "name" => "Citrine",
+        "tr_label" => clienttranslate("Citrine")
     ],
-    // ...
+    3 => [
+        "name" => "Emerald",
+        "tr_label" => clienttranslate("Emerald")
+    ],
+    4 => [
+        "name" => "Sapphire",
+        "tr_label" => clienttranslate("Sapphire")
+    ],
 ];
 
+$this->terrains_info = [
+    1 => [
+        "name" => "Desert",
+        "tr_label" => clienttranslate("Desert")
+    ],
+    2 => [
+        "name" => "Canyon",
+        "tr_label" => clienttranslate("Canyon")
+    ],
+    3 => [
+        "name" => "Florest",
+        "tr_label" => clienttranslate("Florest")
+    ],
+    4 => [
+        "name" => "Ruins",
+        "tr_label" => clienttranslate("Ruins")
+    ],
+    5 => [
+        "name" => "Castle",
+        "tr_label" => clienttranslate("Castle")
+    ],
+];
 
+$this->tileEffects_info = [
+    1 => [
+        "label" => clienttranslate("coin(s)"),
+        "values" => [
+            1 => 1,
+            2 => 2,
+            3 => 3,
+            4 => null
+        ],
+    ],
+    2 => [
+        "label" => clienttranslate("Royalt point(s)"),
+        "values" => [
+            1 => 1,
+            2 => 2,
+            3 => 3,
+            4 => 5
+        ],
+    ],
+    3 => [
+        "label" => clienttranslate("Stone die"),
+        "values" => [
+            1 => 1,
+            2 => 1,
+            3 => 1,
+            4 => 1
+        ],
+    ],
+];
 
+$this->tiles_info = [
+    1 => [
+        "terrain" => 1,
+        "gem" => 0,
+        "effect" => 0,
+    ],
 
+    2 => [
+        "terrain" => 1,
+        "gem" => 1,
+        "effect" => 0,
+    ],
+    3 => [
+        "terrain" => 1,
+        "gem" => 1,
+        "effect" => 0,
+    ],
+    4 => [
+        "terrain" => 1,
+        "gem" => 1,
+        "effect" => 0,
+    ],
+    5 => [
+        "terrain" => 1,
+        "gem" => 2,
+        "effect" => 0,
+    ],
+    6 => [
+        "terrain" => 1,
+        "gem" => 2,
+        "effect" => 0,
+    ],
+    7 => [
+        "terrain" => 1,
+        "gem" => 2,
+        "effect" => 0,
+    ],
+    8 => [
+        "terrain" => 1,
+        "gem" => 3,
+        "effect" => 0,
+    ],
+    9 => [
+        "terrain" => 1,
+        "gem" => 3,
+        "effect" => 0,
+    ],
+    10 => [
+        "terrain" => 1,
+        "gem" => 3,
+        "effect" => 0,
+    ],
+    11 => [
+        "terrain" => 1,
+        "gem" => 4,
+        "effect" => 0,
+    ],
+    12 => [
+        "terrain" => 1,
+        "gem" => 4,
+        "effect" => 0,
+    ],
+    13 => [
+        "terrain" => 1,
+        "gem" => 4,
+        "effect" => 0,
+    ],
 
+    14 => [
+        "terrain" => 2,
+        "gem" => 0,
+        "effect" => 0
+    ],
+    15 => [
+        "terrain" => 2,
+        "gem" => 1,
+        "effect" => 1
+    ],
+    16 => [
+        "terrain" => 2,
+        "gem" => 1,
+        "effect" => 2
+    ],
+    17 => [
+        "terrain" => 2,
+        "gem" => 1,
+        "effect" => 3
+    ],
+    18 => [
+        "terrain" => 2,
+        "gem" => 2,
+        "effect" => 1
+    ],
+    19 => [
+        "terrain" => 2,
+        "gem" => 2,
+        "effect" => 2
+    ],
+    20 => [
+        "terrain" => 2,
+        "gem" => 2,
+        "effect" => 3
+    ],
+    21 => [
+        "terrain" => 2,
+        "gem" => 3,
+        "effect" => 1
+    ],
+    22 => [
+        "terrain" => 2,
+        "gem" => 3,
+        "effect" => 2
+    ],
+    23 => [
+        "terrain" => 2,
+        "gem" => 3,
+        "effect" => 3
+    ],
+    24 => [
+        "terrain" => 2,
+        "gem" => 4,
+        "effect" => 1
+    ],
+    25 => [
+        "terrain" => 2,
+        "gem" => 4,
+        "effect" => 2
+    ],
+    26 => [
+        "terrain" => 2,
+        "gem" => 4,
+        "effect" => 3
+    ],
+    27 => [
+        "terrain" => 3,
+        "gem" => 0,
+        "effect" => 0
+    ],
+    28 => [
+        "terrain" => 3,
+        "gem" => 1,
+        "effect" => 1
+    ],
+    29 => [
+        "terrain" => 3,
+        "gem" => 1,
+        "effect" => 2
+    ],
+    30 => [
+        "terrain" => 3,
+        "gem" => 1,
+        "effect" => 3
+    ],
+    31 => [
+        "terrain" => 3,
+        "gem" => 2,
+        "effect" => 1
+    ],
+    32 => [
+        "terrain" => 3,
+        "gem" => 2,
+        "effect" => 2
+    ],
+    33 => [
+        "terrain" => 3,
+        "gem" => 2,
+        "effect" => 3
+    ],
+    34 => [
+        "terrain" => 3,
+        "gem" => 3,
+        "effect" => 1
+    ],
+    35 => [
+        "terrain" => 3,
+        "gem" => 3,
+        "effect" => 2
+    ],
+    36 => [
+        "terrain" => 3,
+        "gem" => 3,
+        "effect" => 3
+    ],
+    37 => [
+        "terrain" => 3,
+        "gem" => 4,
+        "effect" => 1
+    ],
+    38 => [
+        "terrain" => 3,
+        "gem" => 4,
+        "effect" => 2
+    ],
+    39 => [
+        "terrain" => 3,
+        "gem" => 4,
+        "effect" => 3
+    ],
+    40 => [
+        "terrain" => 4,
+        "gem" => 0,
+        "effect" => 0
+    ],
+    41 => [
+        "terrain" => 4,
+        "gem" => 1,
+        "effect" => 1
+    ],
+    42 => [
+        "terrain" => 4,
+        "gem" => 1,
+        "effect" => 2
+    ],
+    43 => [
+        "terrain" => 4,
+        "gem" => 1,
+        "effect" => 3
+    ],
+    44 => [
+        "terrain" => 4,
+        "gem" => 2,
+        "effect" => 1
+    ],
+    45 => [
+        "terrain" => 4,
+        "gem" => 2,
+        "effect" => 2
+    ],
+    46 => [
+        "terrain" => 4,
+        "gem" => 2,
+        "effect" => 3
+    ],
+    47 => [
+        "terrain" => 4,
+        "gem" => 3,
+        "effect" => 1
+    ],
+    48 => [
+        "terrain" => 4,
+        "gem" => 3,
+        "effect" => 2
+    ],
+    49 => [
+        "terrain" => 4,
+        "gem" => 3,
+        "effect" => 3
+    ],
+    50 => [
+        "terrain" => 4,
+        "gem" => 4,
+        "effect" => 1
+    ],
+    51 => [
+        "terrain" => 4,
+        "gem" => 4,
+        "effect" => 2
+    ],
+    52 => [
+        "terrain" => 4,
+        "gem" => 4,
+        "effect" => 3
+    ],
+    53 => [
+        "terrain" => 5,
+        "gem" => 0,
+        "effect" => 2,
+    ],
+    54 => [
+        "terrain" => 5,
+        "gem" => 1,
+        "effect" => 2,
+    ],
+    55 => [
+        "terrain" => 5,
+        "gem" => 2,
+        "effect" => 2,
+    ],
+    56 => [
+        "terrain" => 5,
+        "gem" => 3,
+        "effect" => 2,
+    ],
+    57 => [
+        "terrain" => 5,
+        "gem" => 4,
+        "effect" => 2,
+    ],
+    58 => [
+        "terrain" => 5,
+        "gem" => 10,
+        "effect" => 2,
+    ]
+];
