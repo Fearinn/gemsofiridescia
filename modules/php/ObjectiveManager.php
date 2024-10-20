24bce4175985945d907929065243e9e8bf7c315e
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2024-10-19 23:34:21 -0300

    refactor: consider wilds for 'most of' objectives

--- modules/php/ObjectiveManager.php
+++ modules/php/ObjectiveManager.php
@@ -24,12 +24,12 @@ class ObjectiveManager
         $leadPlayer = null;
 
         if ($this->id === 1) {
-            $max_coins = 0;
+            $maxCoins = 0;
 
             foreach ($players as $player_id => $player) {
                 $coins = (int) $this->game->getUniqueValueFromDB("SELECT coin from player WHERE player_id=$player_id");
-                if ($coins > $max_coins || ($coins === $max_coins && $player_id === $current_player_id)) {
-                    $max_coins = $coins;
+                if ($coins > $maxCoins || ($coins === $maxCoins && $player_id === $current_player_id)) {
+                    $maxCoins = $coins;
                     $leadPlayer = $player_id;
                 }
             }
@@ -39,50 +39,68 @@ class ObjectiveManager
 
         if ($this->id >= 2 && $this->id <= 5) {
             $gem_id = $this->variable;
-            $max_tiles = 0;
 
+            $currentTiles = $this->game->getStat("$gem_id:GemTiles", $current_player_id);
+            $rainbowTiles = $this->game->getStat("rainbow:Tiles", $current_player_id);
+
+            $maxTiles = 0;
             foreach ($players as $player_id => $player) {
+                if ($player_id === $current_player_id) {
+                    continue;
+                }
+
                 $tilesCount = $this->game->getStat("$gem_id:GemTiles", $player_id);
 
-                if ($tilesCount > $max_tiles || ($tilesCount === $max_tiles && $player_id === $current_player_id)) {
-                    $max_tiles = $tilesCount;
-                    $leadPlayer = $player_id;
+                if ($tilesCount >= $maxTiles) {
+                    $maxTiles = $tilesCount;
                 }
             }
 
-            return $leadPlayer === $current_player_id;
+            return ($currentTiles + $rainbowTiles) >= $maxTiles;
         }
 
         if ($this->id >= 7 && $this->id <= 10) {
             $gem_id = $this->variable;
-            $max_relics = 0;
 
+            $currentRelics = $relicsCount = $this->game->getStat("$gem_id:GemRelics", $current_player_id);
+            $iridiaRelics = $this->game->getStat("iridia:Relics", $current_player_id);
+
+            $maxRelics = 0;
             foreach ($players as $player_id => $player) {
+                if ($player_id === $current_player_id) {
+                    continue;
+                }
+
                 $relicsCount = $this->game->getStat("$gem_id:GemRelics", $player_id);
 
-                if ($relicsCount > $max_relics || ($relicsCount === $max_relics && $player_id === $current_player_id)) {
-                    $max_relics = $relicsCount;
-                    $leadPlayer = $player_id;
+                if ($relicsCount >= $maxRelics) {
+                    $maxRelics = $relicsCount;
                 }
             }
 
-            return $leadPlayer === $current_player_id;
+            return ($currentRelics + $iridiaRelics) >= $maxRelics;
         }
 
         if ($this->id >= 12 && $this->id <= 14) {
             $relicType = $this->variable;
-            $max_relics = 0;
 
+            $currentRelics = $relicsCount = $this->game->getStat("$relicType:TypeRelics", $current_player_id);
+            $iridiaRelics = $this->game->getStat("iridia:Relics", $current_player_id);
+
+            $maxRelics = 0;
             foreach ($players as $player_id => $player) {
+                if ($player_id === $current_player_id) {
+                    continue;
+                }
+
                 $relicsCount = $this->game->getStat("$relicType:TypeRelics", $player_id);
 
-                if ($relicsCount > $max_relics || ($relicsCount === $max_relics && $player_id === $current_player_id)) {
-                    $max_relics = $relicsCount;
-                    $leadPlayer = $player_id;
+                if ($relicsCount >= $maxRelics) {
+                    $maxRelics = $relicsCount;
                 }
             }
 
-            return $leadPlayer === $current_player_id;
+            return ($currentRelics + $iridiaRelics) >= $maxRelics;
         }
 
         if ($this->id === 6) {
