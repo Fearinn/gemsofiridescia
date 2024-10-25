99f266ab507adb3f71248623186fe196f6e5189a
Matheus Gomes <105985983+Fearinn@users.noreply.github.com>
2024-10-24 21:38:39 -0300

    chore (ItemManager): implement method 'isUsable'

--- modules/php/ItemManager.php
+++ modules/php/ItemManager.php
@@ -4,7 +4,8 @@ declare(strict_types=1);
 
 namespace Bga\Games\GemsOfIridescia;
 
-class ItemManager {
+class ItemManager
+{
     public function __construct(int $item_id, \Table $game)
     {
         $this->game = $game;
@@ -13,5 +14,30 @@ class ItemManager {
         $this->id = $item_id;
         $this->tr_name = $info["tr_name"];
         $this->cost = $info["cost"];
+    }
+
+    public function isUsable(int $player_id)
+    {
+        if ($this->id === 1) {
+            return $this->game->getTotalGemsCount($player_id) >= 2;
+        }
+
+        if ($this->id === 3) {
+            return $this->game->getCoins($player_id) >= 3;
+        }
+
+        if ($this->id === 8) {
+            return $this->game->getTotalGemsCount($player_id) >= 1;
+        }
+
+        if ($this->id === 9) {
+            return $this->game->getCoins($player_id) >= 3;
+        }
+
+        if ($this->id === 11) {
+            return !!$this->game->expandedRevealableTiles($player_id) || !!$this->game->expandedExplorableTiles($player_id);
+        }
+
+        return true;
     }
 }
